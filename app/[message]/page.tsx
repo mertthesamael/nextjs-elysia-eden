d53afe4809823c55d17f21182516e15c22bca46c
Mert Enercan <mertenercan@gmail.com>
2024-08-13 17:33:38 +0300

    - buildtime status testing

--- app/[message]/page.tsx
+++ app/[message]/page.tsx
@@ -2,11 +2,11 @@ import { elysia } from "@/elysia/client";
 
 export default async function Page({ params }: { params: { ['message']: string } }) {
 
-    const { data, error } = await elysia.api.message({ "message": params.message }).get()
+    //const { data, error } = await elysia.api.message({ "message": params.message }).get()
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <p className="text-4xl font-bold text-black/80">{data}</p>
+            <p className="text-4xl font-bold text-black/80">{'data'}</p>
         </main>
     );
 }

72caeeb86635ab1f36a87dd84d7d01cb23a6c19e
Mert Enercan <mertenercan@gmail.com>
2024-08-13 17:43:30 +0300

    - minor fix

--- elysia/client.ts
+++ elysia/client.ts
@@ -10,4 +10,4 @@ import { treaty } from '@elysiajs/eden'
 //     },
 //   })
 const url = process.env.URL_DOMAIN ?? "localhost:3000"
-export const elysia = treaty<TElysiaApp>('localhost:3000')
+export const elysia = treaty<TElysiaApp>(url)
